f6a2bfed74659c3938d1d330a1d309d34806eb97
laszekQ <jalexeyvlyakhovichj@Protonmail.com>
2026-06-26 17:33:31 +0200

    First commit

---
+++ src/main/java/com/github/laszekq/titlelibrary/TitleLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.github.laszekq.titlelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitleLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TitleLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/laszekq/titlelibrary/TitleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.laszekq.titlelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitleLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
