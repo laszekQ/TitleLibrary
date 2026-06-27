af7a3eedd4744d842f9277a133180844442a8a45
laszekQ <jalexeyvlyakhovichj@Protonmail.com>
2026-06-27 16:34:36 +0200

    Login Page

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO ROLE (name) VALUES ('User');
 INSERT INTO ROLE (name) VALUES ('Editor');
 INSERT INTO ROLE (name) VALUES ('Admin');
+
+INSERT INTO STATUS (name) VALUES ('Finished');
+INSERT INTO STATUS (name) VALUES ('In Progress');
+INSERT INTO STATUS (name) VALUES ('Planning');
+INSERT INTO STATUS (name) VALUES ('Dropped');
